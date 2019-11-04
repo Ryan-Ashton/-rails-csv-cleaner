2c0a40191f99792881277b6e51acdeb10034ee90
Ryan Ashton <ryan.mark.ashton@gmail.com>
2019-11-04 18:25:41 +0800

    Updated models and controllers to include instance variables

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,16 +1,34 @@
 class FeedbacksController < ApplicationController
+
+  before_action :set_feedback, only: [:show, :edit]
+
+
   def new
   end
 
   def create
+    @feedback = Feedback.new(feedback_params)
+    @feedback.save
   end
 
   def show
+    @feedback = Feedback.find(params[:id])
   end
 
   def index
+    @feedbacks = Feedback.all
   end
 
   def edit
   end
+
+  private
+  def feedback_params
+    params.require(:feedback).permit(:rating, :comments)
+  end
+
+  def set_feedback
+    @feedback = Feedback.find(params[:id])
+  end
+
 end
