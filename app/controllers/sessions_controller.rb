b66f5dfdeca6f78bce0ba381e467483bee5ebf9c
Sebastian Bausch <sebasbeijing@gmail.com>
2019-11-05 18:11:44 +0800

    Dropzone JS uploader

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class SessionsController < ApplicationController
   
   def index
@@ -9,14 +10,46 @@ class SessionsController < ApplicationController
   end
   
   def create
+    
     @session = Session.new(session_params)
+    # byebug
     @session.user = current_user
 
     # Placeholder algorithm
     @session.algorithm = Algorithm.first
 
+    # path = params["file"].tempfile
+
+
+    # CSV.foreach(filepath, csv_options) do |row|
+    #   puts "#{row['Name']}, a #{row['Appearance']} beer from #{row['Origin']}"
+    # end
+
+    # CSV.open(path, 'a') do |csv|
+    #   csv << ['Guinness', 'Stout', 'Ireland']
+    # end
+
+    # csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+
+    # csv_data = CSV.generate(headers: true) do |csv|
+    #   CSV.open(path, 'r', :row_sep => :auto, :col_sep => ";") do |row|
+    #     csv << row
+    #   end
+    # end
+
+    # send_data csv_data, filename: "data-#{Date.today.to_s}.csv", disposition: :attachment
+
+    # make the user download it
+
     @session.save!
-    raise
+    # respond_to do |format|
+    #   format.html { redirect_to sessions_path }
+    #   format.json { render json: @session.to_json }
+    # end
+    respond_to do |format|
+      format.json{ render :json => @session }
+    end
+
   end
 
   def new
@@ -25,6 +58,6 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:file)
+    params.permit(:file)
   end
 end
