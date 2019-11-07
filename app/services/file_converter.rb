23d081ec9a8902fb9741fa87383d5f9456edb83c
Ryan Ashton <ryan.mark.ashton@gmail.com>
2019-11-07 12:08:33 +0800

    added new algorithms to service file

--- app/services/file_converter.rb
+++ app/services/file_converter.rb
@@ -26,4 +26,96 @@ class FileConverter < ApplicationService
     csv_data
   end
 
+  #######################################################
+
+  def header_spaces
+
+    csv_options = { col_sep: ',', quote_char: '"' }
+  
+    lines = []
+  
+    CSV.foreach(@file, csv_options) do | row |
+      lines << row.map { |e| e.gsub(/ +?/, '') }
+    end
+  
+    csv_data = CSV.generate(headers: true) do |csv|
+      lines.each do |line|
+        csv << line
+      end
+    end
+
+    return false unless csv_data
+    csv_data
+  end
+
+
+
+  #######################################################
+
+  def remove_new_lines
+
+    csv_options = { col_sep: ',', quote_char: '"' }
+  
+    lines = []
+  
+    CSV.foreach(@file, csv_options) do | row |
+      lines << row.map { |e| e.gsub(/[\n\r +?]/, '') }
+    end
+  
+    csv_data = CSV.generate(headers: true) do |csv|
+      lines.each do |line|
+        csv << line
+      end
+    end
+
+    return false unless csv_data
+    csv_data
+  end
+
+    #######################################################
+
+    def remove_blank_columns
+
+      csv_options = { col_sep: ',', quote_char: '"' }
+    
+      lines = []
+    
+      CSV.foreach(@file, csv_options) do | row |
+        row = row.compact!
+        lines << row
+      end
+    
+      csv_data = CSV.generate(headers: true) do |csv|
+        lines.each do |line|
+          csv << line
+        end
+      end
+
+      return false unless csv_data
+      csv_data
+    end
+
+    #######################################################
+
+    def white_space_left_right
+
+      csv_options = { col_sep: ',', quote_char: '"' }
+    
+      lines = []
+    
+      CSV.foreach(@file, csv_options) do | row |
+        lines << row.map { |e| e.gsub(/ +?/, '') }
+      end
+    
+      csv_data = CSV.generate(headers: true) do |csv|
+        lines.each do |line|
+          csv << line
+        end
+      end
+
+      return false unless csv_data
+      csv_data
+    end
+
+
 end
