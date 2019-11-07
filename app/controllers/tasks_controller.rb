7d81586a742634c643bbe0b0d574294593bb526f
Ryan Ashton <ryan.mark.ashton@gmail.com>
2019-11-07 14:43:52 +0800

    idea for case when statement

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,22 @@ class TasksController < ApplicationController
     # Placeholder algorithm
     # @task.algorithm = Algorithm.first
 
+    # case algorithm_selection
+    # when task_params[:algorithm_id] = 1
+    #   algo = FileConverter.algorithm_1
+    # when task_params[:algorithm_id] = 2
+    #   algo = FileConverter.header_spaces
+    # when task_params[:algorithm_id] = 3
+    #   algo = FileConverter.remove_new_lines
+    # when task_params[:algorithm_id] = 4
+    #   algo = FileConverter.remove_blank_columns
+    # when task_params[:algorithm_id] = 5
+    #   algo = FileConverter.white_space_left_right
+    # when task_params[:algorithm_id] = 6
+    #   algo = FileConverter.missing_random_data
+    # end
+
+
     if @task.save
       csv_data = FileConverter.algorithm_1(task_params[:file].tempfile.path)
       send_data csv_data, filename: task_params[:file].original_filename + "-fixed.csv", disposition: :attachment
