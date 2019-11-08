fb55ee31dd47edad505cce92868b21f686325420
Ryan Ashton <ryan.mark.ashton@gmail.com>
2019-11-08 16:54:16 +0800

    update

--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,7 @@ module RailsCsvCleaner
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-<<<<<<< HEAD
     #Spring.watch "app/services/**"
-=======
-    # Spring.watch "app/services/**"
->>>>>>> 29edc319aef689ef36e99d580ddb6442f49acae0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
