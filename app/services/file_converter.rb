5cb7aac56a06a41e3acb93057750b83db5f4e0e3
Sebastian Bausch <sebasbeijing@gmail.com>
2019-11-08 12:41:00 +0800

    Enabled all algos

--- app/services/file_converter.rb
+++ app/services/file_converter.rb
@@ -1,14 +1,14 @@
 require 'csv'
 require 'pry-byebug'
 
-class FileConverter < ApplicationService
+class FileConverter
 
   def initialize(file)
     @csv_blob = file
   end
 
-  # Remove symbol algorithm
-  def algorithm_1
+  def remove_symbols
+    begin
     csv_options = { col_sep: ',', quote_char: '"' }
     lines = []
 
@@ -22,19 +22,22 @@ class FileConverter < ApplicationService
       end
     end
 
-    return false unless csv_data
-    csv_data
+    return csv_data
+
+    rescue Exception
+      return false
+    end
   end
 
   #######################################################
 
   def header_spaces
-
+    begin
     csv_options = { col_sep: ',', quote_char: '"' }
 
     lines = []
 
-    CSV.foreach(@file, csv_options) do | row |
+    CSV.parse(@csv_blob.download, csv_options) do | row |
       lines << row.map { |e| e.gsub(/ +?/, '') }
     end
 
@@ -44,8 +47,11 @@ class FileConverter < ApplicationService
       end
     end
 
-    return false unless csv_data
-    csv_data
+    return csv_data
+
+    rescue Exception
+      return false
+    end
   end
 
 
@@ -53,12 +59,12 @@ class FileConverter < ApplicationService
   #######################################################
 
   def remove_new_lines
-
+    begin
     csv_options = { col_sep: ',', quote_char: '"' }
 
     lines = []
 
-    CSV.foreach(@file, csv_options) do | row |
+    CSV.parse(@csv_blob.download, csv_options) do | row |
       lines << row.map { |e| e.gsub(/[\n\r +?]/, '') }
     end
 
@@ -68,19 +74,22 @@ class FileConverter < ApplicationService
       end
     end
 
-    return false unless csv_data
-    csv_data
+    return csv_data
+
+    rescue Exception
+      return false
+    end
   end
 
     #######################################################
 
     def remove_blank_columns
-
+      begin
       csv_options = { col_sep: ',', quote_char: '"' }
 
       lines = []
 
-      CSV.foreach(@file, csv_options) do | row |
+      CSV.parse(@csv_blob.download, csv_options) do | row |
         row = row.compact!
         lines << row
       end
@@ -91,19 +100,22 @@ class FileConverter < ApplicationService
         end
       end
 
-      return false unless csv_data
-      csv_data
+      return csv_data
+
+      rescue Exception
+        return false
+      end
     end
 
     #######################################################
 
     def white_space_left_right
-
+      begin
       csv_options = { col_sep: ',', quote_char: '"' }
 
       lines = []
 
-      CSV.foreach(@file, csv_options) do | row |
+      CSV.parse(@csv_blob.download, csv_options) do | row |
         lines << row.map { |e| e.gsub(/ +?/, '') }
       end
 
@@ -113,31 +125,38 @@ class FileConverter < ApplicationService
         end
       end
 
-      return false unless csv_data
-      csv_data
+      return csv_data
+
+      rescue Exception
+        return false
+      end
     end
 
     #######################################################
 
     def missing_random_data(file_path)
-      clean = CSV.parse(IO.read(file_path), headers: true, skip_blanks: true).delete_if { |row| row.to_hash.values.all?(&:nil?) }
-      
+      begin
+      clean = CSV.parse(@csv_blob.download, headers: true, skip_blanks: true).delete_if { |row| row.to_hash.values.all?(&:nil?) }
+
       lines = []
-    
+
       clean.to_a
-    
+
       clean.each do |row|
         lines << row
       end
-    
+
       csv_data = CSV.generate(headers: true) do |csv|
         lines.each do |line|
           csv << line
         end
       end
 
-      return false unless csv_data
-      csv_data 
+      return csv_data
+
+      rescue Exception
+        return false
+      end
     end
 
 
