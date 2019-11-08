9b317dc8a79dd3107c5b09cd67df0ed9f476fac7
Ryan Ashton <ryan.mark.ashton@gmail.com>
2019-11-08 13:23:46 +0800

    updated algorithm sequence

--- app/services/file_converter.rb
+++ app/services/file_converter.rb
@@ -7,13 +7,18 @@ class FileConverter
     @csv_blob = file
   end
 
-  def remove_symbols
+  ##########################################################
+
+  def missing_random_data
     begin
-    csv_options = { col_sep: ',', quote_char: '"' }
+    clean = CSV.parse(@csv_blob.download, headers: true, skip_blanks: true).delete_if { |row| row.to_hash.values.all?(&:nil?) }
+
     lines = []
 
-    CSV.parse(@csv_blob.download, csv_options) do | row |
-      lines << row.map! { |e| e.gsub(/[^A-Za-z]+/, '')}
+    clean.to_a
+
+    clean.each do |row|
+      lines << row
     end
 
     csv_data = CSV.generate(headers: true) do |csv|
@@ -29,7 +34,7 @@ class FileConverter
     end
   end
 
-  #######################################################
+  #########################################################
 
   def header_spaces
     begin
@@ -54,9 +59,7 @@ class FileConverter
     end
   end
 
-
-
-  #######################################################
+  #####################################################
 
   def remove_new_lines
     begin
@@ -81,7 +84,57 @@ class FileConverter
     end
   end
 
-    #######################################################
+  ########################################################
+
+  def remove_symbols
+    begin
+    csv_options = { col_sep: ',', quote_char: '"' }
+    lines = []
+
+    CSV.parse(@csv_blob.download, csv_options) do | row |
+      lines << row.map! { |e| e.gsub(/[^A-Za-z]+/, '')}
+    end
+
+    csv_data = CSV.generate(headers: true) do |csv|
+      lines.each do |line|
+        csv << line
+      end
+    end
+
+    return csv_data
+
+    rescue Exception
+      return false
+    end
+  end
+
+  #######################################################
+  
+  def white_space_left_right
+    begin
+    csv_options = { col_sep: ',', quote_char: '"' }
+
+    lines = []
+
+    CSV.parse(@csv_blob.download, csv_options) do | row |
+      lines << row.map { |e| e.gsub(/ +?/, '') }
+    end
+
+    csv_data = CSV.generate(headers: true) do |csv|
+      lines.each do |line|
+        csv << line
+      end
+    end
+
+    return csv_data
+
+    rescue Exception
+      return false
+    end
+  end
+
+  #######################################################
+
 
     def remove_blank_columns
       begin
@@ -106,58 +159,7 @@ class FileConverter
         return false
       end
     end
+  end
 
-    #######################################################
+    
 
-    def white_space_left_right
-      begin
-      csv_options = { col_sep: ',', quote_char: '"' }
-
-      lines = []
-
-      CSV.parse(@csv_blob.download, csv_options) do | row |
-        lines << row.map { |e| e.gsub(/ +?/, '') }
-      end
-
-      csv_data = CSV.generate(headers: true) do |csv|
-        lines.each do |line|
-          csv << line
-        end
-      end
-
-      return csv_data
-
-      rescue Exception
-        return false
-      end
-    end
-
-    #######################################################
-
-    def missing_random_data(file_path)
-      begin
-      clean = CSV.parse(@csv_blob.download, headers: true, skip_blanks: true).delete_if { |row| row.to_hash.values.all?(&:nil?) }
-
-      lines = []
-
-      clean.to_a
-
-      clean.each do |row|
-        lines << row
-      end
-
-      csv_data = CSV.generate(headers: true) do |csv|
-        lines.each do |line|
-          csv << line
-        end
-      end
-
-      return csv_data
-
-      rescue Exception
-        return false
-      end
-    end
-
-
-end
