bb9180fcf83fc53ec3d795398bbf1d1e408efb6b
sisi florensia <sisiflorensia@gmail.com>
2019-11-11 12:44:30 +0800

    fix modal login reroute

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,9 @@ import "bootstrap";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { initParticle } from '../components/initParticle';
 import { initClickedTile } from '../components/tiles';
+import $ from 'jquery';
 
+window.$ = $;
 initUpdateNavbarOnScroll();
 initParticle();
 initClickedTile();
